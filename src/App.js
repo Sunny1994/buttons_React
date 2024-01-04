1ab02adadbb0cdabae7268dde3b13b013863cfb7
roshan <reddyroshan@gmail.com>
2024-01-04 18:51:23 +0530

    buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from "react";
+import { FC, PropsWithChildren, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import DescriptionIcon from '@mui/icons-material/Description';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
-export default App;
+// const Icon: FC<PropsWithChildren> = ({ children }) => (
+//   <i className="material-symbols-outlined">{children}</i>
+// );
+
+const App=()=>{
+
+  const [isOpen, setIsopen]= useState(false)
+  return(
+    <div className={`fab ${isOpen? "open":""}`}>
+     <button onClick={()=>setIsopen(!isOpen)}>
+      <AddBoxIcon/>
+     </button>
+
+     <div className="menu">
+         <button>
+           <GroupAddIcon/>
+           <span>Add Users</span>
+         </button>
+
+          <button>
+           <DescriptionIcon/>
+           <span>Documents</span>
+          </button>
+
+          <button>
+            <SettingsIcon/>
+            <span>Settings</span>
+          </button>
+     </div>
+     
+
+    </div>
+  )
+
+
+}
+export default App
